75dd71b9e0b950f3795e34569e6c01d6ddbda523
Dita Rahmane <dita.rahmane.silova@inbox.lv>
2018-08-09 16:01:47 +0300

    chore: Basic testing

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -13,7 +13,7 @@ $(function() {
     * a related set of tests. This suite is all about the RSS
     * feeds definitions, the allFeeds variable in our application.
     */
-    describe('RSS Feeds', function() {
+    describe('RSS Feeds', () => {
         /* This is our first test - it tests to make sure that the
          * allFeeds variable has been defined and that it is not
          * empty. Experiment with this before you get started on
@@ -21,52 +21,122 @@ $(function() {
          * allFeeds in app.js to be an empty array and refresh the
          * page?
          */
-        it('are defined', function() {
+        it('are defined', () => {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
 
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
-         */
+        /* 
+        * Checks if the url is defined and if it's not empty 
+        */
+        it('check if url is defined and not empty', () => {
+            allFeeds.forEach(feed => {
+                // Check if url is defined
+                expect(feed.url).toBeDefined();
+                // Check that url is not empty
+                expect(feed.url.length).not.toBe(0);
+            });
+        });
 
+        /* 
+        *  Checks if the name of url is defined and if it's not empty
+        */
+        it('check if name is defined and not empty', () => {
+            allFeeds.forEach(feed => {
+                // Check if name is defined
+                expect(feed.name).toBeDefined();
+                // Check that name is not empty
+                expect(feed.name.length).not.toBe(0);
+            })
+        })
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
-         */
     });
 
 
-    /* TODO: Write a new test suite named "The menu" */
+    /* New test suite named "The menu" */
+    describe('The menu', () => {
+        // Select the required element
+        const body = document.querySelector('body');
 
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
+        /*
+        * Check if the side-menu by default is hidden 
+        */
+        it('is hidden by default', () => {
+            expect(body.className).toContain('menu-hidden');
+        });
+
+        /* 
+        * Check that when the menu icon is clicked, 
+        * side-menu performs showwing and hiding the siode-menu
+        */
+        it('works on click', () => {
+            const menuIcon = $('.menu-icon-link');
+
+            // Check if menu-hidden class is removed on click
+            menuIcon.click();
+            expect(body.className).not.toContain('menu-hidden');
+
+            // Check if menu-hidden class is added on click
+            menuIcon.click();
+            expect(body.className).toContain('menu-hidden');
+        });
+    });
+
+    /* New test suite named "Initial Entries" */
+    describe('Initial Entries', () => {
+        /*
+        * Check if when loadFeed function is called there is at least one entry.
+        */
+        beforeEach(done => {
+            loadFeed(0, () => {
+                done();
+            });
+        });
+
+        it('check if after loadFeed function is called there is at least one entry', (done) => {
+            const container = document.querySelector('.feed');
+            const entriesLength = container.querySelectorAll('.entry').length;
+
+            expect(entriesLength).not.toBe(0);
+            done();
+        });
+
+    });
+
+    /* New test suite named "New Feed Selection" */
+    describe('New Feed Selection', () => {
+        let currentContentUrl,
+            nextContentUrl,
+            currentContentHTML,
+            nextContentHTML;
+
+        const container = document.querySelector('.feed');
+
+        /* 
+         * Check if the content for new feed changes
          */
+        beforeEach(done => {
+            // Empty the prev.entries
+            $('.feed').empty();
+            
+            loadFeed(0, () => {
+                currentContentUrl = allFeeds[0].url;
+                currentContentHTML = container.innerHTML;
+                done();
+            });
 
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
+            loadFeed(3, () => {
+                nextContentUrl = allFeeds[1].url;
+                nextContentHTML = container.innerHTML;
+                done();
+            });
+        });
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+        it('when the new feed is loaded the content of it is changed', () => {
+            expect(currentContentUrl).not.toBe(nextContentUrl);
+            expect(currentContentHTML).not.toBe(nextContentHTML);
+        })
 
-        /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
-         */
-
-    /* TODO: Write a new test suite named "New Feed Selection" */
-
-        /* TODO: Write a test that ensures when a new feed is loaded
-         * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
-         */
+    });
 }());
